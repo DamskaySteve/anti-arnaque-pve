aa08a3199617a47a0d519a698dd27a46ab618870
DamskaySteve <37937235+DamskaySteve@users.noreply.github.com>
2018-07-03 17:08:47 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,23 +11,24 @@ bot.on ("guildMemberAdd", member => {
 	member.guild.channels.find("name", ":raised_hand:bienvenue:raised_hand:").send(':fire: Un anti-arnaqueur a spawn ! Bienvenue sur Fortnite [FR] PVE !  ')
 })
 
-bot.on('message', message =>{
-    if (message.content ==="!clap"){
-        message.reply("Clap des mains ! :clap: :clap: :clap:");
-        console.log("Commande clap salut faite !")
-    }
+bot.on('message', message => {
+    
+	if (message.content === "!clap")
+        	message.reply("Clap des mains ! :clap: :clap: :clap:");
+   	     	console.log("Commande clap salut faite !")
+    	}
 	
-    if(message.content === prefix + "help"){
-	var help_embed = new Discord.RichEmbed()
-	.setColor("#8F20FE")
-	.setTitle("Aide")
-	.setDescription("Liste des commandes")
-	.addField("!help", "Affiche les commandes du bot")
-	.addField("!clap", "Clap des mains")
-	.setFooter("Menu d'aide - Fortnite [FR] Pve")
-	message.channel.sendMessage(help_embed);
-	console.log("Help command")
-    }
+    	if(message.content === prefix + "help"){
+		var help_embed = new Discord.RichEmbed()
+		.setColor("#8F20FE")
+		.setTitle("Aide")
+		.setDescription("Liste des commandes")
+		.addField("!help", "Affiche les commandes du bot")
+		.addField("!clap", "Clap des mains")
+		.setFooter("Menu d'aide - Fortnite [FR] Pve")
+		message.channel.sendMessage(help_embed);
+		console.log("Help command")
+    	}
 });
 
 bot.login(process.env.TOKEN);
