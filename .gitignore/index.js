ac3e2e77a055c1be37f77ad63a5b000a04e6ca9b
DamskaySteve <37937235+DamskaySteve@users.noreply.github.com>
2018-07-03 14:33:30 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,12 +3,12 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-	bot.user.setGame("Anti-Arnaque.exe");
+	bot.user.setGame("Fortnite FR");
 	console.log("Le bot a ete connecte")
 })
 
 bot.on ("guildMemberAdd", member => {
-	member.guild.channels.find("name", "bienvenue").send(':fire: Un anti-arnaqueur a spawn ! Bienvenue sur Anti-Arnaque PVE !  ')
+	member.guild.channels.find("name", ":raised_hand:bienvenue:raised_hand:").send(':fire: Un anti-arnaqueur a spawn ! Bienvenue sur Fortnite [FR] PVE !  ')
 })
 
 bot.login(process.env.TOKEN);
