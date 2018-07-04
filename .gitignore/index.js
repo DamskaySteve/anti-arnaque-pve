a7b23bd051ee0e5d7a0d8ef0dd419c82cb71afba
DamskaySteve <37937235+DamskaySteve@users.noreply.github.com>
2018-07-04 12:33:28 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,27 +8,27 @@ bot.on("ready", function() {
 })
 
 bot.on ("guildMemberAdd", member => {
-	member.guild.channels.find("name", ":raised_hand:bienvenue:raised_hand:").send(':fire: Un anti-arnaqueur a spawn ! Bienvenue sur Fortnite [FR] PVE !  ')
+	member.guild.channels.find("name", "✋bienvenue✋").send(':fire: Un membre a spawn ! Bienvenue sur Fortnite [FR] PVE !  ')
 })
 
 bot.on('message', message => {
     
-	if (message.content === "!clap"){
-        	message.reply("Clap des mains ! :clap: :clap: :clap:");
-   	     	console.log("Commande clap salut faite !");
-    	}
-	
-    	if(message.content === prefix + "help"){
-		var help_embed = new Discord.RichEmbed()
-		.setColor("#8F20FE")
-		.setTitle("Aide")
-		.setDescription("Liste des commandes")
-		.addField("!help", "Affiche les commandes du bot")
-		.addField("!clap", "Clap des mains")
-		.setFooter("Menu d'aide - Fortnite [FR] Pve")
-		message.channel.sendMessage(help_embed);
-		console.log("Help command")
-    	}
-});
+	if(message.content.startsWith("!sondage")) {
+		let args = message.content.split(" ").slice(1);
+		let thingToEcho = args.join(" ")
+		let embed = new Discord.RichEmbed()
+			.setDescription("**Sondage**")
+			.addField(thingToEcho, "Repondre avec :white_check_mark: ou :x:")
+			.setColor('#551EEC')
+		message.channel.send(embed)
+		.then(function (message) {
+			message.react("✅")
+			message.react("❌")
+		}).catch(function() {
+		});
+
+	}
+})
+
 
 bot.login(process.env.TOKEN);
